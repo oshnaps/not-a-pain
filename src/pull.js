3df19b9d415b52a095e2dfa29ddddc22b9178d39
Shnaps <ortal@interlude.fm>
2017-05-04 16:23:02 +0300

    src initial

---
+++ src/pull.js
@@ -0,0 +1,13 @@
+///// pull.js /////
+
+//// in db: //////
+/*
+areas: major: '', lately: []
+meds: {name1: time1, name2: time2}
+*/
+
+function handle() {
+
+}
+
+module.exports = {handle: handle};
