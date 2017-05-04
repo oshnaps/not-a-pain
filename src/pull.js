b429a0033946d29dbad888d772f99f315c5b8394
Shnaps <ortal@interlude.fm>
2017-05-04 21:04:16 +0300

    pull full structure and start of flow

--- src/pull.js
+++ src/pull.js
@@ -1,13 +1,246 @@
 ///// pull.js /////
+let aws = require('aws-sdk');
+let dynamo = new AWS.DynamoDB.DocumentClient();
 
-//// in db: //////
-/*
-areas: major: '', lately: []
-meds: {name1: time1, name2: time2}
-*/
+let questions = [
+	{
+		label: "0",
+		q: "{{IS_NOT_FIRST_TIME}} Did you remember to take {{MEDS}}?",
+		a: [
+			{
+				value: "Took it",
+				payload: {
+					value: "",
+					goto: 1
+				}
+			}, {
+				value: "Snooze",
+				payload: {
+					value: "Trying again...",
+					goto: 0
+				}
+			}
+		] 
+	},
+	{
+		label: "1",
+		q: "Good! do you feel pain right now?",
+		a: [
+			{
+				value: "Yes",
+				payload: {
+					value: "",
+					goto: 2
+				}
+			}, {
+				value: "No",
+				payload: {
+					value: {
+						user: {
+							pain_level: 0
+						}
+					},
+					goto: 5
+				}
+			}
+		] 
+	},
+	{
+		label: "2",
+		q: "Still in {{MAIN_AREA}}?",
+		a: [
+			{
+				value: "Yes",
+				payload: {
+					value: "",
+					goto: 4
+				}
+			}, {
+				value: "No",
+				payload: {
+					value: "",
+					goto: 3
+				}
+			}
+		] 
+	},
+	{
+		label: "3",
+		q: "Where, than?",
+		a: [""] // body areas
+	},
+	{
+		label: "4",
+		q: "How painful is it?",
+		a: [
+			{
+				value: "A Lot",
+				payload: {
+					value: {
+						user: {
+							pain_level: 1
+						}
+					},
+        			image_url: "http://petersfantastichats.com/img/red.png"
+				}
+			}, {
+				value: "",
+				payload: {
+					value: {
+						user: {
+							pain_level: 2
+						}
+					},
+        			image_url: "http://petersfantastichats.com/img/red.png"
+				}
+			}, {
+				value: "",
+				payload: {
+					value: {
+						user: {
+							pain_level: 3
+						}
+					},
+        			image_url: "http://petersfantastichats.com/img/red.png"
+				}
+			}, {
+				value: "",
+				payload: {
+					value: {
+						user: {
+							pain_level: 4
+						}
+					},
+        			image_url: "http://petersfantastichats.com/img/red.png"
+				}
+			}, {
+				value: "",
+				payload: {
+					value: {
+						user: {
+							pain_level: 5
+						}
+					},
+        			image_url: "http://petersfantastichats.com/img/red.png"
+				}
+			}, {
+				value: "",
+				payload: {
+					value: {
+						user: {
+							pain_level: 6
+						}
+					},
+        			image_url: "http://petersfantastichats.com/img/red.png"
+				}
+			}, {
+				value: "",
+				payload: {
+					value: {
+						user: {
+							pain_level: 7
+						}
+					},
+        			image_url: "http://petersfantastichats.com/img/red.png"
+				}
+			}, {
+				value: "",
+				payload: {
+					value: {
+						user: {
+							pain_level: 8
+						}
+					},
+        			image_url: "http://petersfantastichats.com/img/red.png"
+				}
+			}, {
+				value: "",
+				payload: {
+					value: {
+						user: {
+							pain_level: 9
+						}
+					},
+        			image_url: "http://petersfantastichats.com/img/red.png"
+				}
+			}, {
+				value: "Not at all",
+				payload: {
+					value: {
+						user: {
+							pain_level: 10
+						}
+					},
+        			image_url: "http://petersfantastichats.com/img/red.png"
+				}
+			}
+		] 
+	},
+	{
+		label: "5",
+		q: "{{CHANGE_IN_PAIN}} How do you generally feel?",
+		a: [""] // feelings 
+	},
+	{
+		label: "6",
+		q: "{{FEELING_HERE}} When did you take {{MEDS}}?",
+		a: ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "didn't take"] // times 
+	},
+	{
+		label: "7",
+		q: "And when did you take {{SOS_MEDS}}?",
+		a: ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "didn't take"] // times 
+	},
+	{
+		label: "8",
+		q: "What were you doing in the last several hours?",
+		a: [""] // activities 
+	},
+	{
+		label: "9",
+		q: "Can you please take a picture of the painful area?",
+		a: [""] // activities 
+	}
 
-function handle() {
+
+];
+
+function handle(event) {
+	data.event = event;
+	data.FBUserId = event.sender.id;
+	return getPatientByFBId(data)
+		.then(whichQ)
+		.then(parseAnswer)
+		.then(db.put)
+		.then(sendOutput);
+}
+
+function getPatientByFBId(data) {
+	let params = {
+		TableName: "patients",
+		Key: {
+			HashKey: data.FBUserId
+		}
+	};
+	return new Promise((resolve, reject) => {
+		dynamo.get(params, (err, dat) => {
+			if (err) {
+				console.error("Failed getting patient with error: ", err.message);
+				reject(err);
+			}
+			else {
+				console.log("Got patient data");
+				data.patient = dat.Item;
+				resolve(data);
+			}
+		});
+	});
+}
+
+function whichQ(data) {
+	let currentEntry = data.current;
 
 }
+
 
 module.exports = {handle: handle};
